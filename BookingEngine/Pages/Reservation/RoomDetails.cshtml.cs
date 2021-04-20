ee7817d8e0362b4fd504651870dd0b0b1ef23dbb
Srujana Jami <srujana.jami@innRoad.com>
2021-04-20 15:00:14 +0530

    Commit

--- BookingEngine/Pages/Reservation/RoomDetails.cshtml.cs
+++ BookingEngine/Pages/Reservation/RoomDetails.cshtml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using BookingEngine.Data;
 using BookingEngine.Models;
 
 namespace BookingEngine.Pages.Reservation
